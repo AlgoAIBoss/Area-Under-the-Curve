4167e3ad8bd7d37d1a97154c3a8760c44ef927f9
abbosjon.madiev <abbosjon.madiev_2023@ucentralasia.org>
2021-11-22 09:51:56 +0600

    changed

--- config/settings.py
+++ config/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
+STATIC_ROOT = BASE_DIR.joinpath('static')
 STATIC_URL = "/static/"
 django_heroku.settings(locals())
 # Default primary key field type
